558bf3118b7af6153237f472e5566dae6d308f50
christian-westbrook <christian.a.westbrook@proton.me>
2026-04-10 16:03:02 -0500

    feat(crucible): Initialized a new Rust project with Cargo

---
+++ stirge-engine/crucible/src/main.rs
@@ -0,0 +1,17 @@
+//! Crucible is the open-source MMO server powering Stirge Hunters.
+
+/// ----------------------------------------------------------------------------
+/// System     : Stirge Engine
+/// Service    : Crucible
+/// File       : main.rs
+/// Engineer   : Christian Westbrook
+/// Copyright  : (c) 2026 Christian Westbrook
+/// License    : Distributed under the MIT license. See the LICENSE file in the
+///              repository root for details.
+///
+/// Abstract   : This file provides the entrypoint to the Crucible MMO server.
+/// ----------------------------------------------------------------------------
+
+fn main() {
+    println!("Hello, world!");
+}
