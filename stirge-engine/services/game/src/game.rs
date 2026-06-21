75c98edbe9f96e1887acf7002bd5ca4cb5b4f00c
Christian Westbrook <christian.a.westbrook@proton.me>
2026-06-20 18:11:07 -0700

    feat(game): Extracted tick duration into a constant in game.rs

--- stirge-engine/services/game/src/game.rs
+++ stirge-engine/services/game/src/game.rs
@@ -1,5 +1,9 @@
 //! Orchestrator for the game loop.
 
+// Note that Stirge Hunters gameplay is balanced around this tick rate.
+// Modify at your own risk.
+const TICK_DURATION_IN_MS: u64 = 500;
+
 /// Represents the game loop.
 pub struct Game {}
 
@@ -8,7 +12,7 @@ impl Game {
     /// Starts the game loop.
     pub async fn start(&self) {
 
-        let mut interval = tokio::time::interval(tokio::time::Duration::from_millis(500));
+        let mut interval = tokio::time::interval(tokio::time::Duration::from_millis(TICK_DURATION_IN_MS));
 
         loop {
             interval.tick().await;
